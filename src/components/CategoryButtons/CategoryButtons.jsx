93dfe6cba82d96a48cdd0c846f7053e290ba55e7
Amanda <amanda.froment@hotmail.com>
2022-12-06 10:53:34 -0500

    styled all buttons

--- src/components/CategoryButtons/CategoryButtons.jsx
+++ src/components/CategoryButtons/CategoryButtons.jsx
@@ -7,30 +7,34 @@ export default function CategoryButtons({
 }) {
   return (
     <>
-      <div className="ButtonContainer">
+      <div className="CategoryButtonContainer">
         <button
+          className="dog-meetups"
           value="Dog Meetups"
           onClick={() => setSelectedCategory("Dog Meetups")}
         >
           Dog Group Meetups
         </button>
         <button
+          className="restaurant-crawl"
           value="Restaurant Crawl"
           onClick={() => setSelectedCategory("Restaurant Crawl")}
         >
           Restaurant Crawl
         </button>
         <button
+          className="fitness"
           value="All Things Fitness"
           onClick={() => setSelectedCategory("All Things Fitness")}
         >
           All Things Fitness
         </button>
         <button
+          className="different-event"
           value="A Different Event"
           onClick={() => setSelectedCategory("A Different Event")}
         >
-          I want to create a different event!
+          A Different Event
         </button>
       </div>
     </>
